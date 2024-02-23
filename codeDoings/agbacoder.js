4422b58443661bbc80a7a0a9e10d29d09ceee097
Theo Alali Daminabo <63914949+theoBiebele@users.noreply.github.com>
2024-02-23 00:40:29 -0800

    Add files via upload

---
+++ codeDoings/agbacoder.js
@@ -0,0 +1,29 @@
+const  player= {};
+ const  tag = [];
+ player.name="Theophilus";
+ player.jNumber = "<h1 id=head>Theophilus</h1>";
+ let st= getElementById('head');
+    tag[0]= player.name;
+    tag[1]=player.jNumber;
+    console.log(tag[1]);
+    document.write(tag[0])
+
+
+// document.write(tag[0])
+
+
+
+// const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
+// const one=false;
+// setTimeout(() => {
+//     if (one===false ){
+//        alert("worked");
+// }
+// else {
+//     alert("wow!");
+//     ;}
+// }, 1000);
+// setInterval
+
+
+
